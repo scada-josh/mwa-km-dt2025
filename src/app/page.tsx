15261105585085cfd67b21b29873b246c544f3ae
“Anusorn <scada.josh@gmail.com>
2025-06-29 06:46:31 +0700

    Show Params from .env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly. (MWA KM DT 2025)
+            Save and see your changes instantly. (MWA KM DT 20255) - {process.env.NODE_ENV} : {process.env.APP_NAME}
           </li>
         </ol>
 
