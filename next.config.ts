84c3732ec14bd4f470e5038901e62ba324c3a8d0
“Anusorn <scada.josh@gmail.com>
2025-06-28 22:11:31 +0700

    remove basePath, assetPrefix, trailingsSlash

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/app2',
-  assetPrefix: '/app2',
-  trailingSlash: true,
+  // basePath: '/app2',
+  // assetPrefix: '/app2',
+  // trailingSlash: true,
 };
 
 export default nextConfig;
