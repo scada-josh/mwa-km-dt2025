787da4a2377eb673f2ae964a930e432ad8e0955f
“Anusorn <scada.josh@gmail.com>
2025-06-28 16:36:48 +0700

    github action - pm2 reload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Save and see your changes instantly. (MWA KM DT 2025)
           </li>
         </ol>
 
